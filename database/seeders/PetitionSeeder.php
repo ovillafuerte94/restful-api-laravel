617cceb79fd66cdcb8068a513dfebe7f3a14f67a
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-01-12 16:41:59 -0500

    Petitions infrastructure

--- database/seeders/PetitionSeeder.php
+++ database/seeders/PetitionSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Petition;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PetitionSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            PetitionSeeder::class,
-        ]);
+        Petition::factory()->times(50)->create();
     }
 }
